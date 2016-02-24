88f32f04e72a5dd3cc91ca72ef8fb9b95e83db8d
Ichiwa <thankyou46585960@gmail.com>
2016-02-24 19:12:37 +0900

    :trollface: creating ... ghost development.

---
+++ roles/ghost/files/config.example.js
@@ -0,0 +1,40 @@
+// # Ghost Configuration
+// Setup your Ghost install for various environments
+// Documentation can be found at http://support.ghost.org/config/
+
+var path = require('path'),
+    config;
+
+config = {
+    // ### Production
+    // When running Ghost in the wild, use the production environment
+    // Configure your URL and mail settings here
+    production: {
+        url: '', // required
+        mail: {},
+		database: {
+    		client: 'mysql',
+    		connection: {
+        		host     : '127.0.0.1',
+        		user     : 'ghost',
+        		password : '', // required
+       			database : 'ghost',
+        		charset  : 'utf8'
+    		}
+		},
+        server: {
+            // Host to be passed to node's `net.Server#listen()`
+            host: '127.0.0.1',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            port: '3000'
+        }
+    }
+};
+
+if (config.production.url == '' || config.production.database.connection.password == '') {
+    console.log("url or database password is empty");
+    settimeout( () => process.exit(1), 100);
+}
+
+// Export config
+module.exports = config;
